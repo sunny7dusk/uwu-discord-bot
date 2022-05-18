e276e573655fb895abad45f24861f0540e82a82d
sunny7dusk <nat233947@gmail.com>
2022-05-18 17:32:31 -0400

    first commit

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+import os # default module
+from dotenv import load_dotenv
+from uwu import uwufied
+
+load_dotenv() # load all the variables from the env file
+
+bot = discord.Bot(os.getenv('GUILD'))
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.slash_command(name="uwu", description="UwU translater; for stuterring, use 0.0-1.0", alias=["uwufied"])
+async def uwu(ctx, sentence: discord.Option(str), stuttering=0.2):
+    embed = discord.Embed(
+        color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
+    )
+
+    converted = uwufied(sentence, float(stuttering))
+
+    embed.add_field(name="\u200b",value=converted)
+    await ctx.respond(embed=embed)
+
+bot.run(os.getenv('TOKEN'))
---
+++ uwu.py
@@ -0,0 +1,41 @@
+import random
+
+"""Credit goes to
+https://github.com/python-discord/sir-lancebot/commit/0794f26ea267c355e106366a1226f3e986a682fc
+"""
+UWU_WORDS = {
+    "fi": "fwi",
+    "l": "w",
+    "r": "w",
+    "some": "sum",
+    "th": "d",
+    "thing": "fing",
+    "tho": "fo",
+    "you're": "yuw'we",
+    "your": "yur",
+    "you": "yuw",
+}
+
+def uwufied(text, stuttering):
+    converted = text
+
+    """ This first converts based from Uwu_Words"""
+    for curr in UWU_WORDS.keys():
+        converted = converted.replace(curr, UWU_WORDS[curr])
+    
+    stuterring_converted = ""
+
+    """Given a percentage, we'll determine if we add a stutter or not"""
+    for curr in converted.split(' '):
+        random_num = random.uniform(0, 1)
+        if (random_num<=stuttering):
+            first_char = curr[0]
+            replaced = curr.replace(first_char,first_char+"-"+first_char.lower(),1)
+            stuterring_converted = stuterring_converted + replaced + " "
+        else:
+            stuterring_converted = stuterring_converted + curr + " "
+
+
+    # print(text.translate(str.maketrans(''.join(UWU_WORDS.keys()), ''.join(UWU_WORDS.values()))))
+    # print(stuterring_converted)
+    return stuterring_converted
